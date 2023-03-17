967f61057a5f26b8af46b4cb9ae1a879614d709f
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-17 18:38:18 -0400

    changed name of function

--- public/react/components/PagesList.js
+++ public/react/components/PagesList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Page } from "./Page";
 
-export const PagesList = ({ pages, onClick }) => {
+export const PagesList = ({ pages, fetchPage }) => {
   return (
     <>
       {pages.map((page, idx) => {
-        return <Page page={page} key={idx} onClick={onClick} />;
+        return <Page page={page} key={idx} onClick={fetchPage} />;
       })}
     </>
   );
