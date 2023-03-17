c9ff14ec940aefd092e3a4b20ac4e516ded4b676
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-17 18:38:07 -0400

    added page-title class

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Page = (props) => {
   return (
     <>
-      <h3 onClick={() => props.onClick(props.page.title)}>
+      <h3 className="page-title" onClick={() => props.onClick(props.page.slug)}>
         {props.page.title}
       </h3>
     </>
